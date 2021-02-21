9f7f485123d55b051a180694383a142031bbc4d3
Chris Hutchinson <chris_hutchinson@me.com>
2021-02-21 16:11:38 +0000

    fix: RSS feed validity (#18)
    * fix: RSS feed validity
    
    * More feed fixes

--- pages/api/journal/feed.xml.ts
+++ pages/api/journal/feed.xml.ts
@@ -8,31 +8,28 @@ const feedHandler: NextApiHandler = async (req, res) => {
   const posts = await getPosts();
 
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/xml");
-  res.send(`
-    <?xml version="1.0" ?>
-    <rss version="2.0">
-    <channel>
-      <title>Chris Hutchinson</title>
-      <link>${BASE_DOMAIN}</link>
-      <description>The latest journal entries from Chris Hutchinson – a software engineer, working at The Times, and Raspberry Pi tinkerer.</description>
-     
-      ${posts
-        .map(
-          (post) => `
-        <item>
-          <guid>${post.slug}</guid>
+  res.setHeader("Content-Type", "application/rss+xml");
+  res.send(`<?xml version="1.0" ?>
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>Chris Hutchinson</title>
+    <link>${BASE_DOMAIN}</link>
+    <description>The latest journal entries from Chris Hutchinson – a software engineer, working at The Times, and Raspberry Pi tinkerer.</description>
+    <atom:link href="${BASE_DOMAIN}/api/journal/feed.xml" rel="self" type="application/rss+xml" />
+
+    ${posts
+      .map(
+        (post) => `<item>
+          <guid>${BASE_DOMAIN}/journal/entry/${post.slug}</guid>
           <title>${post.headline}</title>
           <link>${BASE_DOMAIN}/journal/entry/${post.slug}</link>
           <description>${post.summary}</description>
-          <pubDate>${post.publishedAt}</pubDate>
-        </item>
-      `
-        )
-        .join("\n")}
-    </channel>
-    </rss> 
-`);
+          <pubDate>${new Date(post.publishedAt).toUTCString()}</pubDate>
+        </item>`
+      )
+      .join("\n")}
+  </channel>
+  </rss>`);
 };
 
 export default feedHandler;
