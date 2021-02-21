018cee74905b509b56fd16964ff216f30dc5c0fb
Chris Hutchinson <chris_hutchinson@me.com>
2021-02-21 16:10:23 +0000

    More feed fixes

--- pages/api/journal/feed.xml.ts
+++ pages/api/journal/feed.xml.ts
@@ -10,23 +10,22 @@ const feedHandler: NextApiHandler = async (req, res) => {
   res.statusCode = 200;
   res.setHeader("Content-Type", "application/rss+xml");
   res.send(`<?xml version="1.0" ?>
-  <rss version="2.0">
+  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
   <channel>
     <title>Chris Hutchinson</title>
     <link>${BASE_DOMAIN}</link>
     <description>The latest journal entries from Chris Hutchinson – a software engineer, working at The Times, and Raspberry Pi tinkerer.</description>
-    
+    <atom:link href="${BASE_DOMAIN}/api/journal/feed.xml" rel="self" type="application/rss+xml" />
+
     ${posts
       .map(
-        (post) => `
-      <item>
-        <guid>${post.slug}</guid>
-        <title>${post.headline}</title>
-        <link>${BASE_DOMAIN}/journal/entry/${post.slug}</link>
-        <description>${post.summary}</description>
-        <pubDate>${post.publishedAt}</pubDate>
-      </item>
-    `
+        (post) => `<item>
+          <guid>${BASE_DOMAIN}/journal/entry/${post.slug}</guid>
+          <title>${post.headline}</title>
+          <link>${BASE_DOMAIN}/journal/entry/${post.slug}</link>
+          <description>${post.summary}</description>
+          <pubDate>${new Date(post.publishedAt).toUTCString()}</pubDate>
+        </item>`
       )
       .join("\n")}
   </channel>
