3c8214baa2d808d9327a191547dbde83eb66806e
Chris Hutchinson <chris_hutchinson@me.com>
2021-02-21 16:06:51 +0000

    fix: RSS feed validity

--- pages/api/journal/feed.xml.ts
+++ pages/api/journal/feed.xml.ts
@@ -8,31 +8,29 @@ const feedHandler: NextApiHandler = async (req, res) => {
   const posts = await getPosts();
 
   res.statusCode = 200;
-  res.setHeader("Content-Type", "text/xml");
-  res.send(`
-    <?xml version="1.0" ?>
-    <rss version="2.0">
-    <channel>
-      <title>Chris Hutchinson</title>
-      <link>${BASE_DOMAIN}</link>
-      <description>The latest journal entries from Chris Hutchinson – a software engineer, working at The Times, and Raspberry Pi tinkerer.</description>
-     
-      ${posts
-        .map(
-          (post) => `
-        <item>
-          <guid>${post.slug}</guid>
-          <title>${post.headline}</title>
-          <link>${BASE_DOMAIN}/journal/entry/${post.slug}</link>
-          <description>${post.summary}</description>
-          <pubDate>${post.publishedAt}</pubDate>
-        </item>
-      `
-        )
-        .join("\n")}
-    </channel>
-    </rss> 
-`);
+  res.setHeader("Content-Type", "application/rss+xml");
+  res.send(`<?xml version="1.0" ?>
+  <rss version="2.0">
+  <channel>
+    <title>Chris Hutchinson</title>
+    <link>${BASE_DOMAIN}</link>
+    <description>The latest journal entries from Chris Hutchinson – a software engineer, working at The Times, and Raspberry Pi tinkerer.</description>
+    
+    ${posts
+      .map(
+        (post) => `
+      <item>
+        <guid>${post.slug}</guid>
+        <title>${post.headline}</title>
+        <link>${BASE_DOMAIN}/journal/entry/${post.slug}</link>
+        <description>${post.summary}</description>
+        <pubDate>${post.publishedAt}</pubDate>
+      </item>
+    `
+      )
+      .join("\n")}
+  </channel>
+  </rss>`);
 };
 
 export default feedHandler;
