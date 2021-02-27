8c25d151b70e7621119b51c4b3b052476e5a2c34
Chris Hutchinson <chris_hutchinson@me.com>
2021-02-27 08:38:12 +0000

    feat: add Panelbear analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,24 @@
 /** @jsxImportSource theme-ui */
 
-import NextApp from "next/app";
+import { AppProps } from "next/app";
 import { ThemeProvider } from "theme-ui";
 import { config } from "@fortawesome/fontawesome-svg-core";
 
 import theme from "../theme";
+import { usePanelbear } from "../utils/panelbear";
 
 config.autoAddCss = false;
 
-export default class App extends NextApp {
-  render() {
-    const { Component, pageProps } = this.props;
+const PANELBEAR_SITE_ID = "72CeUgcKa4h";
 
-    return (
-      <ThemeProvider theme={theme}>
-        <Component {...pageProps} />
-      </ThemeProvider>
-    );
-  }
-}
+const CustomApp = ({ Component, pageProps }: AppProps) => {
+  usePanelbear(PANELBEAR_SITE_ID);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+};
+
+export default CustomApp;
