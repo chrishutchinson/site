9bbc61d3e5707929c483fe071dfbf650a92ce1ef
Chris Hutchinson <chris_hutchinson@me.com>
2025-08-16 09:20:09 +0100

    install Vercel analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@
 import { AppProps } from "next/app";
 import { ThemeProvider } from "theme-ui";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { Analytics } from "@vercel/analytics/next";
 
 import theme from "../theme";
 
@@ -11,6 +12,7 @@ config.autoAddCss = false;
 const CustomApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider theme={theme}>
+      <Analytics />
       <Component {...pageProps} />
     </ThemeProvider>
   );
