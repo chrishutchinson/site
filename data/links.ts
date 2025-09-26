c1167d97cf73e05b076bf4981b7a98e7d5788da1
Chris Hutchinson <1573022+chrishutchinson@users.noreply.github.com>
2025-09-26 21:12:38 +0100

    feat: about page and CV (#81)
    * feat: add about page
    
    * feat: add past roles

---
+++ data/links.ts
@@ -0,0 +1,15 @@
+export const externalLinks = {
+  twitter: "https://www.twitter.com/chrishutchinson",
+  github: "https://github.com/chrishutchinson",
+  linkedin: "https://www.linkedin.com/in/chris-hutchinson-32b76237/",
+  substack: "https://fringeengineering.substack.com",
+  madeTech: "https://www.madetech.com",
+  learnerbly: "https://www.learnerbly.com",
+  theTimes: "https://www.thetimes.com",
+  raspberryPiTrainDepartureScreenRepo:
+    "https://github.com/chrishutchinson/train-departure-screen",
+  raspberryPiTrainDepartureScreenArticle:
+    "https://magazine.raspberrypi.com/articles/uk-train-departure-screen",
+  christmasTreePainterLightsRepo:
+    "https://github.com/chrishutchinson/christmas-tree-painter",
+};
