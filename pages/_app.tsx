6d86c7c58d1c277f8c5220d544f4a2dbde929587
Chris Hutchinson <chris_hutchinson@me.com>
2025-09-30 07:32:27 +0100

    feat: add speed insights

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@
 
 import { config } from "@fortawesome/fontawesome-svg-core";
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { AppProps } from "next/app";
 import { ThemeUIProvider } from "theme-ui";
 
@@ -14,6 +15,7 @@ const CustomApp = ({ Component, pageProps }: AppProps) => {
     <ThemeUIProvider theme={theme}>
       <Analytics />
       <Component {...pageProps} />
+      <SpeedInsights />
     </ThemeUIProvider>
   );
 };
